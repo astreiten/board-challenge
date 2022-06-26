8ce5cb08529c6b51cb7f68517d06939396af868b
Agustín Streitenberger <a_streiten@outlook.com>
2022-06-26 17:00:09 -0300

    BC005 Implement getClusters logic (#5)

--- src/api/submitBoard.ts
+++ src/api/submitBoard.ts
@@ -1,23 +1,60 @@
 import { BoardType } from "../models/board";
 
+const DFS = (
+  x: number,
+  y: number,
+  board: BoardType[][],
+  visited: boolean[][],
+  cluster: number[][]
+) => {
+  if (
+    x < 0 ||
+    y < 0 ||
+    x >= board[0].length ||
+    y >= board.length ||
+    board[x][y].value === "-" ||
+    visited[x][y]
+  ) {
+    return;
+  }
+
+  cluster.push([x, y]);
+
+  visited[x][y] = true;
+
+  DFS(x + 1, y, board, visited, cluster);
+  DFS(x, y + 1, board, visited, cluster);
+  DFS(x - 1, y, board, visited, cluster);
+  DFS(x, y - 1, board, visited, cluster);
+  return;
+};
+
+const getClusters = (board: BoardType[][]) => {
+  let clusters: number[][][] = [];
+  const rows = new Array(board.length).fill("");
+  const visited: boolean[][] = rows.map(() =>
+    new Array(board[0].length).fill(false)
+  );
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[0].length; j++) {
+      if (board[i][j].value === "+") {
+        if (!visited[i][j]) {
+          let newCluster: number[][] = [];
+          DFS(i, j, board, visited, newCluster);
+          clusters.push(newCluster);
+        }
+      }
+    }
+  }
+  return clusters;
+};
+
 export async function submitBoard(board: BoardType[][]): Promise<number[][][]> {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log(board);
-      resolve([
-        [
-          [1, 0],
-          [2, 0],
-        ],
-        [[3, 2]],
-        [[3, 4]],
-        [[3, 7]],
-        [[5, 8]],
-        [
-          [7, 8],
-          [8, 8],
-        ],
-      ]);
+      const clusters = getClusters(board);
+      console.log(clusters);
+      resolve(clusters);
     }, 3000);
   });
 }
