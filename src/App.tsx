f4823333cb25d52ec41a31d9be999fe45fcf94ab
Agustin Streitenberger <a_streiten@outlook.com>
2022-06-26 20:30:23 -0300

    * UI improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import { Grid, styled, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { getBoard } from "./api/getBoard";
 import { submitBoard } from "./api/submitBoard";
-import "./App.css";
 import { Board } from "./Board";
 import { BoardType } from "./models/board";
 import { buildBoard } from "./utils/buildBoard";
@@ -31,17 +31,44 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      {loading ? (
-        <b>{"loading"}</b>
-      ) : (
-        <Board board={board} setBoard={setBoard} />
-      )}
-      <div>
-        <button onClick={buildClusters}>Submit</button>
-      </div>
-    </div>
+    <Container>
+      <TopBarContainer container justifyContent="center" alignItems="center">
+        <TopBarItem item xs={6}>
+          <Typography variant="h3">Board Challenge</Typography>
+        </TopBarItem>
+      </TopBarContainer>
+      <Grid container justifyContent="center" alignItems="center">
+        {loading ? (
+          <b>{"loading"}</b>
+        ) : (
+          <Grid item xs={12} md={6} lg={10}>
+            {" "}
+            <Board board={board} setBoard={setBoard} />
+            <div>
+              <button onClick={buildClusters}>Submit</button>
+            </div>
+          </Grid>
+        )}
+      </Grid>
+    </Container>
   );
 }
+
+const Container = styled("div")({
+  backgroundColor: "#c8d8e4",
+  height: "100vh",
+  margin: "0",
+});
+
+const TopBarContainer = styled(Grid)({
+  backgroundColor: "#2b6777",
+  height: "5rem",
+});
+
+const TopBarItem = styled(Grid)({
+  height: "5rem",
+  textAlign: "center",
+  marginTop: "1rem",
+});
 
 export default App;
